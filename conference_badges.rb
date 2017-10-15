c0c797ae8af226b8de07e8a5f6aed20b87948439
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-15 00:28:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index do |attendee|
+  attendees.each_with_index do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{attendees.index + 1}!"
   end
 end
