5f33e5faaf38b4f2dcd0ebc76c88eee83dd769a0
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-15 00:56:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  batch_badge_creator(attendees).each do |message|
+    puts message
+  end
+  assign_rooms(attendees).each do |message|
+    puts message
+  end
 end
