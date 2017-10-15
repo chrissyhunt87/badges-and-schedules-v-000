16307847b52d7e72a6b67f71ddbaf96876129203
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-15 00:26:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,6 @@ end
 
 def assign_rooms(attendees)
   attendees.map do |attendee|
-    "Hello, #{name}! You'll be assigned to room #{attendees.index + 1}"!""
+    "Hello, #{name}! You'll be assigned to room #{attendees.index + 1}!"
   end
 end
